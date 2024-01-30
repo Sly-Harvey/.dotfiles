1a1ec02af853ac47547fb54de1e0ed0ffdd1e662
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-01-30 20:47:59 +0000

    remove errors

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "[*] Cloning submodules..."
 git submodule update --init
 
 set -e
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-DOT_FOLDERS="bin,alacritty,cava,dunst,gwe,hypr,kitty,lf,MangoHud,neofetch,ranger,rofi,swww,tmux,neovim,zsh"
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOT_FOLDERS="bin,alacritty,cava,dunst,gwe,hypr,kitty,lf,MangoHud,neofetch,ranger,rofi,swww,tmux,neovim,bash,zsh"
 
 if ! command -v stow &> /dev/null; then
     if command -v apt &> /dev/null; then
@@ -16,13 +16,16 @@ if ! command -v stow &> /dev/null; then
 fi
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
-    echo "[+] Folder :: $folder"
+    # echo "[+] SYMLINK :: $folder"
 
-    stow -t $HOME -D $folder \
-        --ignore=README.md --ignore=LICENSE 
-    stow -t $HOME $folder
+    stow -t $HOME -D $folder -v \
+        --ignore=README.md --ignore=LICENSE \
+        2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
+    stow -t $HOME $folder -v \
+        2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2)
 done
 
 # Reload shell once installed
 echo "[*] Reloading shell..."
 exec $SHELL -l
+echo "[*] Done!"
--- uninstall.sh
+++ uninstall.sh
@@ -3,6 +3,12 @@
 DOT_FOLDERS=*/
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
-    echo "[+] $folder"
-    stow -D -v -t $HOME $folder
+    stow -D -v -t $HOME $folder -v \
+        2> >(grep -v 'BUG in find_stowed_path? Absolute/relative mismatch' 1>&2) \
+        2> >(grep -v 'Planning unstow of package' 1>&2) \
+        2> >(grep -v 'stow dir path relative to target' 1>&2) \
+        2> >(grep -v 'stow dir is' 1>&2) \
+        2> >(grep -v 'Processing tasks' 1>&2)
+        # 2> >(grep -v 'UNLINK' 1>&2)
+    # echo "[-] UNLINK :: $folder"
 done
