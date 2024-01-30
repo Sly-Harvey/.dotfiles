347cf44653f962c6c549e18828d643ff54bbf218
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-01-30 23:46:15 +0000

    added more package managers

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
+# Find package manager
+package_manager="brew install"
+declare -A osInfo;
+osInfo[/etc/arch-release]="sudo pacman --noconfirm -S"
+osInfo[/etc/fedora-release]="sudo dnf install -y"
+osInfo[/etc/centos-release]="sudo yum install -y"
+osInfo[/etc/gentoo-release]="sudo emerge"
+osInfo[/etc/SuSE-release]="sudo zypper install"
+osInfo[/etc/debian_version]="sudo apt-get install -y"
+osInfo[/etc/alpine-release]="sudo apk --update add"
+for f in ${!osInfo[@]}
+do
+    if [[ -f $f ]];then
+        package_manager=${osInfo[$f]}
+    fi
+done
+
+if ! command -v stow &> /dev/null; then
+    $package_manager "stow"
+fi
+
 echo "[*] Cloning submodules..."
 git submodule update --init
 
 set -e
 # SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DOT_FOLDERS="bin,alacritty,cava,dunst,gwe,hypr,kitty,lf,MangoHud,neofetch,ranger,rofi,swww,tmux,neovim,bash,zsh"
-
-if ! command -v stow &> /dev/null; then
-    if command -v apt &> /dev/null; then
-        sudo apt install -y stow
-    else
-        sudo pacman -S stow
-    fi
-fi
 
 for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
     # echo "[+] SYMLINK :: $folder"
