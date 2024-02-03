d862a83a5b64985bb4a222355d3f4e3bb8a36896
Harvey Jacobs-Grant <mheharvey@gmail.com>
2024-02-03 00:17:41 +0000

    removed false error

--- uninstall.sh
+++ uninstall.sh
@@ -9,6 +9,7 @@ for folder in $(echo $DOT_FOLDERS | sed "s/,/ /g"); do
         2> >(grep -v 'stow dir path relative to target' 1>&2) \
         2> >(grep -v 'stow dir is' 1>&2) \
         2> >(grep -v 'Processing tasks' 1>&2) \
+        2> >(grep -v 'did not exist to be unstowed' 1>&2) \
         2> >(grep -v 'firefox' 1>&2)
         # 2> >(grep -v 'UNLINK' 1>&2)
     # echo "[-] UNLINK :: $folder"
